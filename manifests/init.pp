3f436bcb4471f3c11a25ec8772dad2cec30e4f49
Nick Walker <nick.walker@puppetlabs.com>
2015-11-17 10:04:21 -0800

    Allow configuring the frequency of the run in hours

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,11 @@
-class flush_jruby_pool {
+class flush_jruby_pool (
+  $cron_hour = '*/4'
+) {
 
   cron { 'flush_puppetserver_jruby_pool' :
     command => template('flush_jruby_pool/curl_to_flush_jruby_pool.erb'),
     user    => 'root',
-    hour    => '*/4',
+    hour    => $cron_hour,
     minute  => '0',
   }
 
