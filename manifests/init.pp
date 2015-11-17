21ad1657cc9dc85133bf3a8696cffa0d4fb6e442
Nick Walker <nick.walker@puppetlabs.com>
2015-11-17 10:23:20 -0800

    Make the minute the cronjob runs on random per host

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,11 +2,14 @@ class flush_jruby_pool (
   $cron_hour = '*/4'
 ) {
 
+  #prefer a random minute so not all masters flush at the same time
+  $random_minute = fqdn_rand(60)
+
   cron { 'flush_puppetserver_jruby_pool' :
     command => template('flush_jruby_pool/curl_to_flush_jruby_pool.erb'),
     user    => 'root',
     hour    => $cron_hour,
-    minute  => '0',
+    minute  => $random_minute,
   }
 
 }
